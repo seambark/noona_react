75d23cdd482169fe8abcb62160d7b2b191fa806e
Seam Park <run128@naver.com>
2024-03-22 00:51:15 +0900

    style link 추가

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import RockPaperScissorClass from './page/RockPaperScissorClass';
 
 export default class AppClass extends Component {
